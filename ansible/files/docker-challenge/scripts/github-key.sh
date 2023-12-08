56a337fa3bae92286448406e6ea4bc8ce5961b06
Leon Jacobs <leonja511@gmail.com>
2023-12-08 09:28:16 +0200

    (feat) allow keys from either github or gitlab to be used
    format to ssh would now be ssh -p24 'user@github'@remotehost

--- ansible/files/docker-challenge/scripts/github-key.sh
+++ ansible/files/docker-challenge/scripts/github-key.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-# download a users key from Github for the AuthorizedKeysCommand
+# download a users key from Github/Gitlab for the AuthorizedKeysCommand
 
 if [ -z "$1" ]; then
 	echo "a user argument is required"
 	exit 1
 fi
 
-KEY=$(echo -n $(curl -fsSL https://github.com/$1.keys))
+USER=$(echo "$1" | cut -d'@' -f1)
+SERVICE=$(echo "$1" | cut -d'@' -f2)
+
+if [ -z "$USER" ]; then
+	echo "empty user"
+	exit 1
+fi
+
+if [ -z "$SERVICE" ]; then
+	echo "empty service"
+	exit 1
+fi
+
+KEY=$(curl -fsSL https://$SERVICE.com/$USER.keys)
 
 if [ $? -ne 0 ]; then
+	echo "key curl failed"
 	exit 1
 fi
 
