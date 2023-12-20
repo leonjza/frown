6f176d06869a7601a5a509c030101ea4c62f0b95
Leon Jacobs <leonja511@gmail.com>
2023-12-20 11:39:25 +0200

    (feat) avoid a possible redherring when segfaulting
    using something like the following frida-trace command will cause
    the game to crash for some reason:
     frida-trace -H localhost -i 'libc.so.6!*' Gadget
    
    The output will include the plumbing script that wraps tetris in
    timeout, possibly causing players to spent time on something that
    is not relevant to the challenge. To avoid that I added some
    comments that will end up in the output of the game to steer them
    away.

--- ansible/files/docker-challenge/scripts/tetris.sh
+++ ansible/files/docker-challenge/scripts/tetris.sh
@@ -2,4 +2,10 @@
 
 # run tetris for 8 hours maximum. this will help clean up stale containers
 # that lost it's SSH connection while also not being too frustrating.
-/usr/bin/timeout --foreground --preserve-status 8h /usr/local/bin/tetris
+
+/usr/bin/timeout \
+	`# if you can see this, this is not a memory corruption challenge` \
+	`# the command you are seeing now is simply challenge plumbing` \
+	--foreground \
+	--preserve-status 8h \
+	/usr/local/bin/tetris
